6e6c39eae4256775a86dfa8657e06b5786d7e15b
unknown <tetd4rthli13@gmail.com>
2024-09-04 17:55:45 +0900

    via commit

--- src/model_har.py
+++ src/model_har.py
@@ -4,18 +4,15 @@ import torch.nn as nn
 class BasicBlock1D(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1, downsample=None, residual_conv=False):
+    def __init__(self, in_planes, planes, stride=1, padding=1, downsample=None):
         super(BasicBlock1D, self).__init__()
-        self.conv1 = nn.Conv1d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv1d(in_planes, planes, kernel_size=3, stride=stride, padding=padding, bias=False)
         self.bn1 = nn.BatchNorm1d(planes)
         self.relu = nn.ReLU(inplace=True)
-        self.dropout = nn.Dropout(0.4)
-        self.conv2 = nn.Conv1d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv1d(planes, planes, kernel_size=3, stride=1, padding=padding, bias=False)
         self.bn2 = nn.BatchNorm1d(planes)
         self.downsample = downsample
         self.stride = stride
-        self.resconv = nn.Conv1d(in_planes, planes, kernel_size=3, stride=2, padding=1, bias=False)
-        self.residual_conv = residual_conv
 
     def forward(self, x):
         identity = x
@@ -24,16 +21,12 @@ class BasicBlock1D(nn.Module):
         out = self.bn1(out)
         out = self.relu(out)
         
-        # out = self.dropout(out)
         out = self.conv2(out)
         out = self.bn2(out)
 
         if self.downsample is not None:
-            identity = self.downsample(x)
-            
-        if self.residual_conv:
-            identity = self.resconv(x)
-            
+            identity = self.downsample(identity)
+
         out += identity
         out = self.relu(out)
         
@@ -41,30 +34,30 @@ class BasicBlock1D(nn.Module):
 
 # HAR用のsmall_resnetを定義
 class ResNet1D_small(nn.Module):
-    def __init__(self, block, layers, num_classes):
+    def __init__(self, block, layers, num_classes, num_channel=1):
         super(ResNet1D_small, self).__init__()
         self.in_planes = 64
-        self.conv1 = nn.Conv1d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.conv1 = nn.Conv1d(num_channel, 64, kernel_size=5, stride=2, padding=2, bias=False)
         self.bn1 = nn.BatchNorm1d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
-        self.layer1a = self._make_layer(block, 64, layers[0])
-        self.layer1b = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer2a = self._make_layer(block, 256, layers[2], stride=2, residual_conv=True)
-        self.layer2b = self._make_layer(block, 512, layers[3], stride=2)
+        self.layer1a = self._make_layer(block, 64, layers[0], stride=1, padding=1)
+        self.layer1b = self._make_layer(block, 64, layers[1], stride=1, padding=1)
+        self.layer2a = self._make_layer(block, 128, layers[2], stride=2, padding=1)
+        self.layer2b = self._make_layer(block, 128, layers[3], stride=1, padding=1)
         self.avgpool = nn.AdaptiveAvgPool1d(1)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.fc = nn.Linear(128, num_classes)
 
-    def _make_layer(self, block, planes, blocks, stride=1, residual_conv=False):
+    def _make_layer(self, block, planes, blocks, stride=1, padding=1):
         downsample = None
         if stride != 1 or self.in_planes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv1d(self.in_planes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm1d(planes * block.expansion),
+                nn.Conv1d(self.in_planes, planes * block.expansion, kernel_size=1, stride=stride, padding=0, bias=False),
+                # nn.BatchNorm1d(planes * block.expansion),
             )
 
         layers = []
-        layers.append(block(self.in_planes, planes, stride, downsample, residual_conv=residual_conv))
+        layers.append(block(self.in_planes, planes, stride, padding, downsample))
         self.in_planes = planes * block.expansion
         for _ in range(1, blocks):
             layers.append(block(self.in_planes, planes))
